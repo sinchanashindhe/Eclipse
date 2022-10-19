d453d6093c0bfd636715f8a4d025e894bd3ea373
unknown <sinchanashindhe11@gmail.com>
2022-10-19 14:27:22 +0530

    eclipse

--- TextileShopRunner.java
+++ TextileShopRunner.java
@@ -1,37 +1,37 @@
 package variables.com.xworkz.programs.Eclipse;
-import variables.com.xworkz.programs.Eclipse.TextileShopRunner;
+import variables.com.xworkz.programs.boot.TextTile;
 
 
 public class TextileShopRunner {
 
 	public static void main(String[] args) {
 		
-		TextileShop textileShop=new TextileShop("Raghavendra");
+		TextTile textileShop=new TextTile("Raghavendra");
 		System.out.println(textileShop.OwnerName);
 
-		TextileShop textileShop1=new TextileShop("Raghavendra", "Ramya");
+		TextTile textileShop1=new TextTile("Raghavendra", "Ramya");
 		System.out.println(textileShop1.OwnerName);
 		System.out.println(textileShop1.OwnerWifeName);
 		
-		TextileShop textileShop2=new TextileShop("Raghavendra", "Ramya", "Vanitha");
+		TextTile textileShop2=new TextTile("Raghavendra", "Ramya", "Vanitha");
 		System.out.println(textileShop2.OwnerName);
 		System.out.println(textileShop2.OwnerWifeName);
 		System.out.println(textileShop2.OwnerDaughterName);
 		
-		TextileShop textileShop3=new TextileShop("Raghavendra", "Ramya", "Vanitha", 9723435637L);
+		TextTile textileShop3=new TextTile("Raghavendra", "Ramya", "Vanitha", 9723435637L);
 		System.out.println(textileShop3.OwnerName);
 		System.out.println(textileShop3.OwnerWifeName);
 		System.out.println(textileShop3.OwnerDaughterName);
 		System.out.println(textileShop3.OwnerDaughterNumber);
 		
-		TextileShop textileShop4=new TextileShop("Raghavendra", "Ramya", "Vanitha", 9723435637L,3);
+		TextTile textileShop4=new TextTile("Raghavendra", "Ramya", "Vanitha", 9723435637L,3);
 		System.out.println(textileShop4.OwnerName);
 		System.out.println(textileShop4.OwnerWifeName);
 		System.out.println(textileShop4.OwnerDaughterName);
 		System.out.println(textileShop4.OwnerDaughterNumber);
 		System.out.println(textileShop4.OwnersNoofWifes);
 		
-		TextileShop textileShop5=new TextileShop("Raghavendra", "Ramya", "Vanitha", 9723435637L ,3 ,45);
+		TextTile textileShop5=new TextTile("Raghavendra", "Ramya", "Vanitha", 9723435637L ,3 ,45);
 		System.out.println(textileShop5.OwnerName);
 		System.out.println(textileShop5.OwnerWifeName);
 		System.out.println(textileShop5.OwnerDaughterName);
@@ -39,7 +39,7 @@ public class TextileShopRunner {
 		System.out.println(textileShop5.OwnersNoofWifes);
 		System.out.println(textileShop5.ShopNo);
 		
-		TextileShop textileShop6=new TextileShop("Raghavendra", "Ramya", "Vanitha", 9723435637L ,3 ,45,6362456312L);
+		TextTile textileShop6=new TextTile("Raghavendra", "Ramya", "Vanitha", 9723435637L ,3 ,45,6362456312L);
 		System.out.println(textileShop6.OwnerName);
 		System.out.println(textileShop6.OwnerWifeName);
 		System.out.println(textileShop6.OwnerDaughterName);
@@ -48,7 +48,7 @@ public class TextileShopRunner {
 		System.out.println(textileShop6.ShopNo);
 		System.out.println(textileShop6.ContactNo);
 		
-		TextileShop textileShop7=new TextileShop("Raghavendra", "Ramya", 3);
+		TextTile textileShop7=new TextTile("Raghavendra", "Ramya", 3);
 		System.out.println(textileShop7.OwnerName);
 		System.out.println(textileShop7.OwnerWifeName);
 		System.out.println(textileShop7.OwnersNoofWifes);
